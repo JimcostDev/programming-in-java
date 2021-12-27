11783dd5b17d98523ba1cd67ba83d5b3907177b2
JimcostDev <rjak727@gmail.com>
2021-12-27 17:48:49 -0500

    Codigo del ejemplo

---
+++ 04_POO/01_CLASES/Persona.java
@@ -0,0 +1,62 @@
+class Persona {
+  // Atributos
+  private String nombre;
+  private int edad;
+  private float peso;
+  private float estatura;
+
+  // Metodos (comportamiento)
+  // Setters
+  void setNombre(String n) {
+    if (n == null || n == "") {
+      System.out.println("El nombre no puede ser vacio");
+    }
+    if (n.length() <= 1) {
+      System.out.println("El nombre no puede ser tan corto");
+    } else {
+      this.nombre = n;
+    }
+
+  }
+
+  void setEdad(int e) {
+    if (e < 0 || e > 150) {
+      System.out.println("La edad no es válida");
+    } else {
+      this.edad = e;
+    }
+  }
+
+  void setPeso(float p){
+      if (p < 0 || p > 300) {
+      System.out.println("El peso no es valido");
+    } else {
+      this.edad = p;
+    }
+  }
+
+  void setEstatura(float es){
+    if (p < 0 || es > 3.0f) {
+    System.out.println("El tamaño no es valido");
+  } else {
+    this.edad = es;
+  }
+}
+
+  // Getters
+  String getNombre() {
+    return this.nombre;
+  }
+
+  int getEdad() {
+    return this.edad;
+  }
+
+  float getPeso (){
+      return this.peso;
+  }
+
+  float getEstatura (){
+    return this.estatura;
+}
+}
---
+++ 04_POO/01_CLASES/Principal.java
@@ -0,0 +1,17 @@
+class Principal{
+    public static void main(String[] args) {
+    // instancia de una clase
+    Persona persona = new Persona();
+    // acceder a los atributos
+    persona.setNombre("Ronaldo Jimenez Acosta");
+    persona.setEdad(21);
+    persona.setPeso(60f);
+    perosona.setEstatura(1.86f)
+
+    // mostrar el objeto por consola
+    System.out.println("Nombre: " + persona.getNombre());
+    System.out.println("Edad: " + persona.getEdad());
+    System.out.println("Peso: " + persona.getPeso());
+    System.out.println("Estatura " + persona.getEstatura());
+  }
+}
