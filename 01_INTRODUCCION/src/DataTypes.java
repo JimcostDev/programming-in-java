e46c1f9fea69f60c59d75d03f18b6e6b6a05a3fb
JimcostDev <rjak727@gmail.com>
2024-02-28 23:59:58 +0100

    agregue la carpeta src, esta contiene el codigo fuente visto en clase

---
+++ 01_INTRODUCCION/src/DataTypes.java
@@ -0,0 +1,41 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // enteros
+        byte age = 27;
+        short year = 2021;
+        int salary = 100000;
+        long population = 1000000000L; // L para indicar que es un long
+        
+        // decimales
+        float price = 100.50F; // F para indicar que es un float
+        double pi = 3.14159;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(salary);
+        System.out.println(population);
+        System.out.println(price);
+        System.out.println(pi);
+
+        // booleanos
+        boolean isOld = true;
+        boolean isYoung = false;
+        System.out.println(!isOld);
+        System.out.println(isYoung);
+
+        // caracteres
+        char yes = 'Y';
+        char no = 'N';  
+
+        System.out.println(yes);
+        System.out.println(no);
+
+        // cadenas
+        String name = "Ronaldo Jimenez Acosta";
+        System.out.println(name);
+        
+
+
+    }
+    
+}
---
+++ 01_INTRODUCCION/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // aqui debe ir mi codigo
+        var username = "jimcostdev";
+        System.out.println(username);
+
+        var $email = "jimcostdev@gmail.com";
+        var _id = 12789;
+        var fullName = "Ronaldo Jimenez Acosta";
+        System.out.println($email + " " + _id);
+        System.out.println(fullName);
+        
+    }
+}
